a4b359a02375b6118fbc9807b7b72920fe54f9b3
Ludovico Piero <lewdovico@gnuweeb.org>
2024-06-02 07:27:15 +0900

    none-ls: add c/c++ & c# formatter
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/lsp/none-ls.nix
+++ config/plugins/lsp/none-ls.nix
@@ -4,18 +4,35 @@
     enable = true;
     sources = {
       formatting = {
+        # C / C++
+        clang_format.enable = true;
+
+        # C-sharp
+        csharpier.enable = true;
+
+        # Nix
         nixfmt = {
           enable = true;
           package = pkgs.nixfmt-rfc-style;
         };
+
+        # Lua
         stylua.enable = true;
+
+        # Bash / SH
         shfmt.enable = true;
+
+        # Python
         black.enable = true;
         isort.enable = true;
+
+        # JS Family
         prettier = {
           disableTsServerFormatter = true;
           enable = true;
         };
+
+        # Go
         gofmt.enable = true;
       };
     };
