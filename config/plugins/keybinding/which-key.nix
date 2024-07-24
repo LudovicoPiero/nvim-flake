69abec370157e632b23defac4cd764e40b19340a
Ludovico Piero <lewdovico@gnuweeb.org>
2024-07-25 04:05:23 +0900

    which-key: disable registrations
    Waiting for nixvim upstream to fix
    
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/keybinding/which-key.nix
+++ config/plugins/keybinding/which-key.nix
@@ -1,17 +1,17 @@
 {
   plugins.which-key = {
     enable = true;
-    registrations = {
-      "<leader>c" = "[C]ode";
-      "<leader>d" = "[D]ocument";
-      "<leader>f" = "[F]ormat";
-      "<leader>g" = "[G]it";
-      "<leader>h" = "Git [H]unk";
-      "<leader>p" = "[P]review";
-      "<leader>r" = "[R]ename";
-      "<leader>s" = "[S]earch";
-      "<leader>t" = "[T]oggle";
-      "<leader>w" = "[W]orkspace";
-    };
+    # registrations = {
+    #   "<leader>c" = "[C]ode";
+    #   "<leader>d" = "[D]ocument";
+    #   "<leader>f" = "[F]ormat";
+    #   "<leader>g" = "[G]it";
+    #   "<leader>h" = "Git [H]unk";
+    #   "<leader>p" = "[P]review";
+    #   "<leader>r" = "[R]ename";
+    #   "<leader>s" = "[S]earch";
+    #   "<leader>t" = "[T]oggle";
+    #   "<leader>w" = "[W]orkspace";
+    # };
   };
 }
