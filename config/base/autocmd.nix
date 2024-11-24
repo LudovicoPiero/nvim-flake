4074991e65279d9c9f80e88d9fc4377cd0f52d7d
Ludovico Piero <lewdovico@gnuweeb.org>
2024-11-24 23:15:51 +0900

    autocmd: add more stuff
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/base/autocmd.nix
+++ config/base/autocmd.nix
@@ -2,17 +2,34 @@
   config = {
     autoCmd = [
       {
-        # jumps to the line of the last edit
-        event = ["BufReadPost"];
+        # Automatically reload files changed outside Neovim
+        event = ["FocusGained" "BufEnter"];
         pattern = ["*"];
+        command = "checktime";
+      }
+      {
+        # Remove trailing whitespace before saving
+        event = ["BufWritePre"];
+        pattern = ["*"];
+        command = "%s/\\s\\+$//e";
+      }
+      {
+        # Auto-insert text for Git commit messages
+        event = ["FileType"];
+        pattern = ["gitcommit"];
         command = ''
-          " Check if the current file is a Git commit message
-          if !exists('b:git_commit')
-            if line("'\"") > 1 && line("'\"") <= line("$")
-              exe "normal! g`\""
-            endif
+          " Start the commit message with a bullet point for convention
+          if line("$") == 1 && getline(1) == ""
+            call append(0, "- ")
+            normal! G
           endif
         '';
+      }
+      {
+        # Disable line numbers and enable spellcheck for Git commit messages
+        event = ["FileType"];
+        pattern = ["gitcommit"];
+        command = "setlocal nonumber norelativenumber spell";
       }
     ];
   };
