af686b81947f11691dbf4269ff9613653c708367
Ludovico Piero <lewdovico@gnuweeb.org>
2024-11-26 16:44:48 +0900

    plugins/lsp: enable inlayHint
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -7,6 +7,7 @@
   plugins = {
     lsp = {
       enable = true;
+      inlayHints = true;
       capabilities = ''
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
