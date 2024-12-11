983433aae1ee7b3494b148469a49166a19365a99
Ludovico Piero <lewdovico@gnuweeb.org>
2024-12-11 11:59:25 +0900

    fix colorizer
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

---
+++ config/plugins/ui/colorizer.nix
@@ -0,0 +1,31 @@
+{
+  plugins.colorizer = {
+    enable = true;
+    settings = {
+      filetypes = {};
+      buftypes = {};
+      user_commands = true;
+      user_default_options = {
+        RGB = true;
+        RRGGBB = true;
+        names = true;
+        RRGGBBAA = true;
+        AARRGGBB = true;
+        rgb_fn = true;
+        hsl_fn = true;
+        css = true;
+        css_fn = true;
+        mode = "background";
+        tailwind = true;
+        sass = {
+          enable = true;
+          parsers = ["css"];
+        };
+        virtualtext = "■";
+        virtualtext_inline = false;
+        virtualtext_mode = "foreground";
+        always_update = false;
+      };
+    };
+  };
+}
--- config/plugins/ui/default.nix
+++ config/plugins/ui/default.nix
@@ -1,12 +1,12 @@
 {
   imports = [
     ./bufferline.nix
+    ./colorizer.nix
     ./colorscheme.nix
     ./web-devicons.nix
     ./notify.nix
     ./noice.nix
     ./rainbow-delimiters.nix
     ./indent-blankline.nix
-    ./nvim-colorizer.nix
   ];
 }
