8f58203294628c13809bf671a98aff30b55a43d7
Ludovico Piero <lewdovico@gnuweeb.org>
2025-01-26 07:34:19 +0900

    utilities/copilot: fix warning
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/utilities/copilot.nix
+++ config/plugins/utilities/copilot.nix
@@ -3,9 +3,11 @@
     copilot-lua = {
       enable = true;
 
-      # Needed for copilot-cmp to work.
-      panel.enabled = false;
-      suggestion.enabled = false;
+      settings = {
+        # Needed for copilot-cmp to work.
+        panel.enabled = false;
+        suggestion.enabled = false;
+      };
     };
 
     copilot-cmp.enable = true;
