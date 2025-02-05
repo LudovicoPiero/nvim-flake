91b8dbb58a22e368da04309a1e69cf54e557ed97
Ludovico Piero <lewdovico@gnuweeb.org>
2025-02-05 15:18:14 +0900

    refactor(notify): restructure plugin configuration under settings
    Refactor the configuration for the notify plugin to use a nested settings structure.
    
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/ui/notify.nix
+++ config/plugins/ui/notify.nix
@@ -1,18 +1,20 @@
 {
   plugins.notify = {
     enable = true;
-    timeout = 3000;
-    backgroundColour = "#000000";
-    maxHeight = {
-      __raw = "math.floor(vim.o.lines * 0.75)";
+    settings = {
+      timeout = 3000;
+      background_colour = "#000000";
+      max_height = {
+        __raw = "math.floor(vim.o.lines * 0.75)";
+      };
+      max_width = {
+        __raw = "math.floor(vim.o.columns * 0.75)";
+      };
+      on_open = ''
+        function(win)
+          vim.api.nvim_win_set_config(win, { zindex = 100 })
+        end
+      '';
     };
-    maxWidth = {
-      __raw = "math.floor(vim.o.columns * 0.75)";
-    };
-    onOpen = ''
-      function(win)
-        vim.api.nvim_win_set_config(win, { zindex = 100 })
-      end
-    '';
   };
 }
