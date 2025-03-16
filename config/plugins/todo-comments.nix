6ee105048ad2dcb44b772937c50f6d71d128d699
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-17 00:26:04 +0900

    plugins: replace todo-comments with mini.hipatterns
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/todo-comments.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, helpers, ... }:
-{
-  pkg = pkgs.vimPlugins.todo-comments-nvim;
-  event = "VimEnter";
-  dependencies = [ pkgs.vimPlugins.plenary-nvim ];
-  opts = helpers.mkRaw ''
-    { signs = false }
-  '';
-}
