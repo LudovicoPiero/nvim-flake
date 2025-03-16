d6828eced7f95e32e33e29a36bf8d4b7f44089e1
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-16 21:08:24 +0900

    bufferline: styling
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/bufferline.nix
+++ config/plugins/bufferline.nix
@@ -5,8 +5,28 @@
   dependencies = with pkgs.vimPlugins; [ nvim-web-devicons ];
   config = ''
     function()
-      require("bufferline").setup {
-        highlights = require("catppuccin.groups.integrations.bufferline").get()
+      local bufferline = require('bufferline')
+      bufferline.setup {
+        highlights = require("catppuccin.groups.integrations.bufferline").get(),
+        options = {
+          show_close_icon = false,
+          show_buffer_close_icons = false,
+          separator_style = "thin",
+          diagnostics = "nvim_lsp",
+          themable = true,
+          offsets = {
+            {
+              filetype = "neo-tree",
+              text = "File Explorer",
+              text_align = "Directory",
+              separator = true
+            }
+          },
+          pick = {
+            alphabet = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ1234567890",
+          },
+
+        },
       }
     end
   '';
