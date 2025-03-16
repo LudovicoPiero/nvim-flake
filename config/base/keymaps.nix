a4508dedc3aeec2254efa9f91799ce4ea2138873
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-16 22:45:46 +0900

    mini: fix keymaps
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/base/keymaps.nix
+++ config/base/keymaps.nix
@@ -87,5 +87,49 @@
       action = "<C-w><C-k>";
       options.desc = "Move focus to the upper window";
     }
+
+    # For mini.nvim
+    {
+      action.__raw = ''
+        function()
+          vim.g.minipairs_disable = not vim.g.minipairs_disable
+        end
+      '';
+      key = "<leader>tp";
+      options.desc = "Toggle auto pairs";
+    }
+    {
+      action.__raw = ''
+        function()
+          local bd = require("mini.bufremove").delete
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+            if choice == 1 then -- Yes
+              vim.cmd.write()
+              bd(0)
+            elseif choice == 2 then -- No
+              bd(0, true)
+            end
+          else
+            bd(0)
+          end
+        end
+      '';
+      key = "<leader>bd";
+      options = {
+        desc = "Delete Buffer";
+      };
+    }
+    {
+      action.__raw = ''
+        function()
+          require("mini.bufremove").delete(0, true)
+        end
+      '';
+      key = "<leader>bD";
+      options = {
+        desc = "Delete Buffer (Force)";
+      };
+    }
   ];
 }
--- config/plugins/mini.nix
+++ config/plugins/mini.nix
@@ -1,14 +1,8 @@
-{ pkgs, helpers, ... }:
+{ pkgs, ... }:
 {
   pkg = pkgs.vimPlugins.mini-nvim;
   config = ''
     function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
       require('mini.bufremove').setup()
@@ -40,8 +34,6 @@
       })
 
       -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = false }
 
@@ -53,45 +45,5 @@
         return '%2l:%-2v'
       end
     end
-  '';
-
-  keys = helpers.mkRaw ''
-    {
-      {
-        "<leader>bd",
-        function()
-          local bd = require("mini.bufremove").delete
-          if vim.bo.modified then
-            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
-            if choice == 1 then -- Yes
-              vim.cmd.write()
-              bd(0)
-            elseif choice == 2 then -- No
-              bd(0, true)
-            end
-          else
-            bd(0)
-          end
-        end,
-        desc = "[F]ormat buffer",
-        mode = "n",
-      },
-      {
-        "<leader>bD",
-        function()
-          require("mini.bufremove").delete(0, true)
-        end,
-        desc = "[D]elete [B]uffer (Force)",
-        mode = "n",
-      },
-      {
-        "<leader>tp",
-        function()
-          vim.g.minipairs_disable = not vim.g.minipairs_disable
-        end,
-        desc = "[T]oggle Auto [P]airs",
-        mode = "n",
-      },
-    }
   '';
 }
