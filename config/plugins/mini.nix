fa2287cf38743b8e0b0f52f95f52a34e12d1dd76
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-20 22:03:45 +0900

    plugins: replace mini.statusline with lualine
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/mini.nix
+++ config/plugins/mini.nix
@@ -31,18 +31,6 @@
           update_n_lines = 'gsn',
         },
       })
-
-      -- Simple and easy statusline.
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = false }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
     end
   '';
 }
