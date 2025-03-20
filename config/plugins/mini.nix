1fc0efe77f035170fb6b8e2a01d62207c36c6915
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-20 22:45:22 +0900

    mini: formatting
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/mini.nix
+++ config/plugins/mini.nix
@@ -4,10 +4,8 @@
   config = ''
     function()
       require('mini.ai').setup { n_lines = 500 }
-
-      require('mini.splitjoin').setup()
-
       require('mini.bufremove').setup()
+      require('mini.splitjoin').setup()
     end
   '';
 }
