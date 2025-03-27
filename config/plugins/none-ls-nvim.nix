54a95bec40b11d869065d60b11afe80d137eab1e
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-28 00:41:40 +0900

    none-ls-nvim: remove comments
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/none-ls-nvim.nix
+++ config/plugins/none-ls-nvim.nix
@@ -45,19 +45,6 @@
             command = "${pkgs.gn}/bin/gn",
           }),
         },
-
-        -- on_attach = function(client, bufnr)
-        --   if client.supports_method("textDocument/formatting") then
-        --     vim.api.nvim_clear_autocmds({ group = vim.api.nvim_create_augroup("LspFormatting", {}), buffer = bufnr })
-        --     vim.api.nvim_create_autocmd("BufWritePre", {
-        --       group = vim.api.nvim_create_augroup("LspFormatting", {}),
-        --       buffer = bufnr,
-        --       callback = function()
-        --         vim.lsp.buf.format({ async = false })
-        --       end,
-        --     })
-        --   end
-        -- end,
       })
     end
   '';
