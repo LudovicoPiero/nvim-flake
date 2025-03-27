e09e23ad27ebc5d0cf9ccac3f55023a93d269682
Ludovico Piero <lewdovico@gnuweeb.org>
2025-03-28 00:25:29 +0900

    nvim-surround: replace with mini.surround
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/mini.nix
+++ config/plugins/mini.nix
@@ -6,6 +6,18 @@
       require('mini.ai').setup { n_lines = 500 }
       require('mini.bufremove').setup()
       require('mini.splitjoin').setup()
+
+      require('mini.surround').setup {
+        mappings = {
+          add = "gsa", -- Add surrounding in Normal and Visual modes
+          delete = "gsd", -- Delete surrounding
+          find = "gsf", -- Find surrounding (to the right)
+          find_left = "gsF", -- Find surrounding (to the left)
+          highlight = "gsh", -- Highlight surrounding
+          replace = "gsr", -- Replace surrounding
+          update_n_lines = "gsn", -- Update `n_lines`
+        },
+      }
     end
   '';
 }
