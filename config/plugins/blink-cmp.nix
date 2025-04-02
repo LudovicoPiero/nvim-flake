80591bb74d809314b60eceb2db4b9f01c0190353
Ludovico Piero <lewdovico@gnuweeb.org>
2025-04-02 15:42:22 +0900

    blink-cmp: only show buffer completion after typing 5 characters
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/blink-cmp.nix
+++ config/plugins/blink-cmp.nix
@@ -58,163 +58,168 @@
   ];
 
   config = ''
-    function()
-      local cmp_status_ok, cmp = pcall(require, "cmp")
-      if not cmp_status_ok then
-        return
-      end
-      local snip_status_ok, luasnip = pcall(require, "luasnip")
-      if not snip_status_ok then
-        return
-      end
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      local has_words_before = function()
-        local col = vim.api.nvim_win_get_cursor(0)[2]
-        if col == 0 then
-          return false
+      function()
+        local cmp_status_ok, cmp = pcall(require, "cmp")
+        if not cmp_status_ok then
+          return
         end
-        local line = vim.api.nvim_get_current_line():sub()
-        return line:sub(col, col):match("%s") == nil
+        local snip_status_ok, luasnip = pcall(require, "luasnip")
+        if not snip_status_ok then
+          return
+        end
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        local has_words_before = function()
+          local col = vim.api.nvim_win_get_cursor(0)[2]
+          if col == 0 then
+            return false
+          end
+          local line = vim.api.nvim_get_current_line():sub()
+          return line:sub(col, col):match("%s") == nil
+        end
+
+        require("blink.cmp").setup({
+          cmdline = { enabled = false },
+
+          keymap = {
+            preset = "super-tab",
+            ['<C-y>'] = { 'select_and_accept' },
+
+            -- preset = "none",
+            -- ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+            -- ["<C-e>"] = { "hide", "fallback" },
+            -- ["<CR>"] = { "accept", "fallback" },
+
+            -- ["<Up>"] = { "select_prev", "fallback" },
+            -- ["<Down>"] = { "select_next", "fallback" },
+            -- ["<Tab>"] = { "select_next", "fallback" },
+            -- ["<S-Tab>"] = { "select_prev", "fallback" },
+
+            -- ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+            -- ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+            -- ["<C-p>"] = { "snippet_forward", "fallback_to_mappings" },
+            -- ["<C-n>"] = { "snippet_backward", "fallback_to_mappings" },
+
+            -- ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+          },
+
+          completion = {
+            keyword = { range = "full" },
+            list = { selection = { preselect = false }, cycle = { from_top = false } },
+
+            menu = {
+              auto_show = true,
+
+              border = "rounded";
+              draw = {
+                treesitter = { "lsp" },
+                columns = {
+                  { "label", "label_description", gap = 1 },
+                  { "kind_icon", "kind" },
+                },
+              },
+            },
+
+            documentation = { auto_show = true, auto_show_delay_ms = 300 },
+
+            ghost_text = { enabled = true },
+          },
+
+          sources = {
+            default = { "lsp", "lazydev", "path", "snippets", "buffer", "calc", "copilot" },
+            providers = {
+              buffer = {
+                min_keyword_length = function()
+                  return vim.bo.filetype == 'markdown' and 0 or 5
+                end
+              },
+              lsp = { score_offset = 5 },
+              snippets = {
+                score_offset = 4,
+                opts = {
+                  use_show_condition = true,
+                  show_autosnippets = true,
+                },
+              },
+              path = {
+                score_offset = 3,
+                opts = {
+                  trailing_slash = true,
+                  label_trailing_slash = true,
+                  get_cwd = function(context)
+                    return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+                  end,
+                  show_hidden_files_by_default = false,
+                },
+              },
+              copilot = {
+                enabled = true,
+                name = "copilot",
+                module = "blink-cmp-copilot",
+                score_offset = 100,
+                async = true,
+              },
+              calc = {
+                enabled = true,
+                name = "calc",
+                module = "blink.compat.source",
+                score_offset = 100,
+                async = true,
+              },
+              lazydev = {
+                name = "LazyDev",
+                module = "lazydev.integrations.blink",
+                score_offset = 100,
+              },
+            },
+          },
+
+          -- Use a preset for snippets, check the snippets documentation for more information
+          snippets = { preset = "luasnip" },
+
+          -- Experimental signature help support
+          signature = { enabled = true },
+
+          fuzzy = { prebuilt_binaries = { download = false } },
+
+          appearance = {
+            -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+            kind_icons = {
+              Copilot = " ",
+              Text = "󰉿 ",
+              Method = "󰊕 ",
+              Function = "󰊕 ",
+              Constructor = "󰒓 ",
+
+              Field = "󰜢 ",
+              Variable = "󰆦 ",
+              Property = "󰖷 ",
+
+              Class = "󱡠 ",
+              Interface = "󱡠 ",
+              Struct = "󱡠 ",
+              Module = "󰅩 ",
+
+              Unit = "󰪚 ",
+              Value = "󰦨 ",
+              Enum = "󰦨 ",
+              EnumMember = "󰦨 ",
+
+              Keyword = "󰻾 ",
+              Constant = "󰏿 ",
+
+              Snippet = "󱄽 ",
+              Color = "󰏘 ",
+              File = "󰈔 ",
+              Reference = "󰬲 ",
+              Folder = "󰉋 ",
+              Event = "󱐋 ",
+              Operator = "󰪚 ",
+              TypeParameter = "󰬛 ",
+            },
+          },
+        })
       end
-
-      require("blink.cmp").setup({
-        cmdline = { enabled = false },
-
-        keymap = {
-          preset = "super-tab",
-          ['<C-y>'] = { 'select_and_accept' },
-
-          -- preset = "none",
-          -- ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-          -- ["<C-e>"] = { "hide", "fallback" },
-          -- ["<CR>"] = { "accept", "fallback" },
-
-          -- ["<Up>"] = { "select_prev", "fallback" },
-          -- ["<Down>"] = { "select_next", "fallback" },
-          -- ["<Tab>"] = { "select_next", "fallback" },
-          -- ["<S-Tab>"] = { "select_prev", "fallback" },
-
-          -- ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-          -- ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-          -- ["<C-p>"] = { "snippet_forward", "fallback_to_mappings" },
-          -- ["<C-n>"] = { "snippet_backward", "fallback_to_mappings" },
-
-          -- ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-        },
-
-        completion = {
-          keyword = { range = "full" },
-          list = { selection = { preselect = false }, cycle = { from_top = false } },
-
-          menu = {
-            auto_show = true,
-
-            border = "rounded";
-            draw = {
-              treesitter = { "lsp" },
-              columns = {
-                { "label", "label_description", gap = 1 },
-                { "kind_icon", "kind" },
-              },
-            },
-          },
-
-          documentation = { auto_show = true, auto_show_delay_ms = 300 },
-
-          ghost_text = { enabled = true },
-        },
-
-        sources = {
-          default = { "lsp", "lazydev", "path", "snippets", "buffer", "calc", "copilot" },
-          providers = {
-            lsp = { score_offset = 5 },
-            snippets = {
-              score_offset = 4,
-              opts = {
-                use_show_condition = true,
-                show_autosnippets = true,
-              },
-            },
-            path = {
-              score_offset = 3,
-              opts = {
-                trailing_slash = true,
-                label_trailing_slash = true,
-                get_cwd = function(context)
-                  return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-                end,
-                show_hidden_files_by_default = false,
-              },
-            },
-            copilot = {
-              enabled = true,
-              name = "copilot",
-              module = "blink-cmp-copilot",
-              score_offset = 100,
-              async = true,
-            },
-            calc = {
-              enabled = true,
-              name = "calc",
-              module = "blink.compat.source",
-              score_offset = 100,
-              async = true,
-            },
-            lazydev = {
-              name = "LazyDev",
-              module = "lazydev.integrations.blink",
-              score_offset = 100,
-            },
-          },
-        },
-
-        -- Use a preset for snippets, check the snippets documentation for more information
-        snippets = { preset = "luasnip" },
-
-        -- Experimental signature help support
-        signature = { enabled = true },
-
-        fuzzy = { prebuilt_binaries = { download = false } },
-
-        appearance = {
-          -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
-          kind_icons = {
-            Copilot = " ",
-            Text = "󰉿 ",
-            Method = "󰊕 ",
-            Function = "󰊕 ",
-            Constructor = "󰒓 ",
-
-            Field = "󰜢 ",
-            Variable = "󰆦 ",
-            Property = "󰖷 ",
-
-            Class = "󱡠 ",
-            Interface = "󱡠 ",
-            Struct = "󱡠 ",
-            Module = "󰅩 ",
-
-            Unit = "󰪚 ",
-            Value = "󰦨 ",
-            Enum = "󰦨 ",
-            EnumMember = "󰦨 ",
-
-            Keyword = "󰻾 ",
-            Constant = "󰏿 ",
-
-            Snippet = "󱄽 ",
-            Color = "󰏘 ",
-            File = "󰈔 ",
-            Reference = "󰬲 ",
-            Folder = "󰉋 ",
-            Event = "󱐋 ",
-            Operator = "󰪚 ",
-            TypeParameter = "󰬛 ",
-          },
-        },
-      })
-    end
   '';
 }
