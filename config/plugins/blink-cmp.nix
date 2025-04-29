0ba70945e0f0c2ce37b1c73865611d5edf2b1e7e
Ludovico Piero <lewdovico@gnuweeb.org>
2025-04-30 00:17:46 +0900

    blink-cmp: change keymap preset to default
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/blink-cmp.nix
+++ config/plugins/blink-cmp.nix
@@ -70,26 +70,7 @@
         cmdline = { enabled = false },
 
         keymap = {
-          preset = "super-tab",
-          ["<C-y>"] = { "select_and_accept" },
-
-          -- preset = "none",
-          -- ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-          -- ["<C-e>"] = { "hide", "fallback" },
-          -- ["<CR>"] = { "accept", "fallback" },
-
-          -- ["<Up>"] = { "select_prev", "fallback" },
-          -- ["<Down>"] = { "select_next", "fallback" },
-          -- ["<Tab>"] = { "select_next", "fallback" },
-          -- ["<S-Tab>"] = { "select_prev", "fallback" },
-
-          -- ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-          -- ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-          -- ["<C-p>"] = { "snippet_forward", "fallback_to_mappings" },
-          -- ["<C-n>"] = { "snippet_backward", "fallback_to_mappings" },
-
-          -- ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+          preset = "default",
         },
 
         completion = {
