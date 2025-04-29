ca0c5acc2017b4157af138ad34334c650ab6f931
Ludovico Piero <lewdovico@gnuweeb.org>
2025-04-30 00:30:21 +0900

    flake: clean up inputs
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
-    emmylua.url = "github:CppCXY/emmylua-analyzer-rust";
-    emmylua.inputs.nixpkgs.follows = "nixpkgs";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -16,14 +17,18 @@
 
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.inputs.flake-parts.follows = "flake-parts";
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    emmylua.url = "github:CppCXY/emmylua-analyzer-rust";
+    emmylua.inputs.nixpkgs.follows = "nixpkgs";
 
     # Plugins
     #TODO: remove after merged
     # blink-pairs.url = "github:Saghen/blink.pairs";
     blink-pairs.url = "github:xarvex/blink.pairs/nix-nightly-toolchain";
     blink-pairs.inputs.nixpkgs.follows = "nixpkgs";
+    blink-pairs.inputs.fenix.follows = "fenix";
+    blink-pairs.inputs.flake-parts.follows = "flake-parts";
   };
 
   outputs =
