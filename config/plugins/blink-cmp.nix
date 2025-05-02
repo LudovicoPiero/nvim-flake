ecc1e6a39a292a97e6d54b92b7b3a8736e365b73
Ludovico Piero <lewdovico@gnuweeb.org>
2025-05-03 07:30:36 +0900

    blink-cmp: move config to opts
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/blink-cmp.nix
+++ config/plugins/blink-cmp.nix
@@ -5,16 +5,21 @@
 }:
 {
   pkg = pkgs.vimPlugins.blink-cmp;
-  event = "InsertEnter";
+  event = [
+    "InsertEnter"
+    "CmdlineEnter"
+  ];
   dependencies = with pkgs.vimPlugins; [
     blink-cmp-copilot
-    friendly-snippets
-    luasnip
+    blink-cmp-conventional-commits
+    blink-nerdfont-nvim
     cmp-calc
+    friendly-snippets
+    lazydev-nvim
     lspkind-nvim
+    luasnip
     {
       pkg = blink-compat;
-      lazy = true;
       opts = { };
     }
     {
@@ -45,142 +50,144 @@
     }
   ];
 
-  config = ''
-    function()
-      local cmp_status_ok, cmp = pcall(require, "cmp")
-      if not cmp_status_ok then
-        return
-      end
-      local snip_status_ok, luasnip = pcall(require, "luasnip")
-      if not snip_status_ok then
-        return
-      end
-      require("luasnip.loaders.from_vscode").lazy_load()
+  opts = helpers.mkRaw ''
+    {
+      cmdline = { enabled = false },
+      keymap = {
+        preset = "default",
+      },
 
-      local has_words_before = function()
-        local col = vim.api.nvim_win_get_cursor(0)[2]
-        if col == 0 then
-          return false
-        end
-        local line = vim.api.nvim_get_current_line():sub()
-        return line:sub(col, col):match("%s") == nil
-      end
-
-      require("blink.cmp").setup({
-        cmdline = { enabled = false },
-
-        keymap = {
-          preset = "default",
+      appearance = {
+        nerd_font_variant = "mono",
+        use_nvim_cmp_as_default = true,
+        kind_icons = {
+          Copilot = "",
         },
+      },
 
-        completion = {
-          keyword = { range = "full" },
-          list = { selection = { preselect = false }, cycle = { from_top = false } },
-
-          menu = {
-            auto_show = true,
-
-            border = "rounded",
-            draw = {
-              treesitter = { "lsp" },
-              columns = {
-                { "label", "label_description", gap = 1 },
-                { "kind_icon", "kind" },
-              },
-              components = {
-                kind_icon = {
-                  text = function(ctx)
-                    local lspkind = require("lspkind")
-                    local icon = ctx.kind_icon
-                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                      local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                      if dev_icon then
-                        icon = dev_icon
-                      end
-                    else
-                      icon = lspkind.symbolic(ctx.kind, {
-                        mode = "symbol",
-                      })
+      completion = {
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        ghost_text = { enabled = true },
+        menu = {
+          auto_show = true,
+          border = "rounded",
+          draw = {
+            treesitter = { "lsp" },
+            columns = {
+              { "label", "label_description", gap = 1 },
+              { "kind_icon", "kind" },
+            },
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local lspkind = require("lspkind")
+                  local icon = ctx.kind_icon
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then
+                      icon = dev_icon
                     end
+                  else
+                    icon = lspkind.symbolic(ctx.kind, {
+                      mode = "symbol",
+                    })
+                  end
 
-                    return icon .. ctx.icon_gap
-                  end,
-
-                  -- Optionally, use the highlight groups from nvim-web-devicons
-                  -- You can also add the same function for `kind.highlight` if you want to
-                  -- keep the highlight groups in sync with the icons.
-                  highlight = function(ctx)
-                    local hl = ctx.kind_hl
-                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                      local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                      if dev_icon then
-                        hl = dev_hl
-                      end
-                    end
-                    return hl
-                  end,
-                },
-              },
-            },
-          },
-
-          documentation = { auto_show = true, auto_show_delay_ms = 300 },
-
-          ghost_text = { enabled = true },
-        },
-
-        sources = {
-          default = { "lsp", "path", "snippets", "buffer", "calc", "copilot" },
-          providers = {
-            buffer = {
-              min_keyword_length = function()
-                return vim.bo.filetype == "markdown" and 0 or 3
-              end,
-            },
-            lsp = { score_offset = 5 },
-            snippets = {
-              score_offset = 4,
-              opts = {
-                use_show_condition = true,
-                show_autosnippets = true,
-              },
-            },
-            path = {
-              score_offset = 3,
-              opts = {
-                trailing_slash = true,
-                label_trailing_slash = true,
-                get_cwd = function(context)
-                  return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+                  return icon .. ctx.icon_gap
                 end,
-                show_hidden_files_by_default = false,
+
+                -- Optionally, use the highlight groups from nvim-web-devicons
+                -- You can also add the same function for `kind.highlight` if you want to
+                -- keep the highlight groups in sync with the icons.
+                highlight = function(ctx)
+                  local hl = ctx.kind_hl
+                  if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                    local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                    if dev_icon then
+                      hl = dev_hl
+                    end
+                  end
+                  return hl
+                end,
               },
-            },
-            copilot = {
-              enabled = true,
-              name = "copilot",
-              module = "blink-cmp-copilot",
-              score_offset = 100,
-              async = true,
-            },
-            calc = {
-              enabled = true,
-              name = "calc",
-              module = "blink.compat.source",
-              score_offset = 100,
-              async = true,
             },
           },
         },
+      },
 
-        -- Use a preset for snippets, check the snippets documentation for more information
-        snippets = { preset = "luasnip" },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "calc", "copilot", "lazydev", "conventional_commits", "nerdfont" },
+        providers = {
+          lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+          conventional_commits = {
+            module = "blink-cmp-conventional-commits",
+            name = "Conventional Commits",
+            enabled = function()
+              return vim.bo.filetype == "gitcommit"
+            end,
+            opts = {},
+          },
+          nerdfont = {
+            module = "blink-nerdfont",
+            name = "Nerd Fonts",
+            score_offset = 15, -- Tune by preference
+            opts = { insert = true }, -- Insert nerdfont icon (default) or complete its name
+          },
+          buffer = {
+            min_keyword_length = function()
+              return vim.bo.filetype == "markdown" and 0 or 1
+            end,
+          },
+          lsp = { score_offset = 4 },
+          snippets = {
+            score_offset = 4,
+            opts = {
+              use_show_condition = true,
+              show_autosnippets = true,
+            },
+          },
+          path = {
+            score_offset = 3,
+            opts = {
+              trailing_slash = true,
+              label_trailing_slash = true,
+              get_cwd = function(context)
+                return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+              end,
+              show_hidden_files_by_default = false,
+            },
+          },
+          copilot = {
+            enabled = true,
+            name = "copilot",
+            module = "blink-cmp-copilot",
+            score_offset = 100,
+            async = true,
+          },
+          calc = {
+            enabled = true,
+            name = "calc",
+            module = "blink.compat.source",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
 
-        -- Experimental signature help support
-        signature = { enabled = true },
+      snippets = { preset = "luasnip" },
 
-        fuzzy = { prebuilt_binaries = { download = false } },
-      })
-    end
+      fuzzy = {
+        implementation = "rust",
+        prebuilt_binaries = {
+          download = false,
+        },
+      },
+
+      signature = {
+        enabled = true,
+        window = { border = "rounded" },
+      },
+    }
   '';
+
 }
