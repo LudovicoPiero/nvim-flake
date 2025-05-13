466f8d5052f67f59e4a3838836a5d8dfdfab0795
Ludovico Piero <lewdovico@gnuweeb.org>
2025-05-13 13:42:31 +0900

    nvim-lspconfig/nixd: use import <nixpkgs> {}
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- config/plugins/nvim-lspconfig.nix
+++ config/plugins/nvim-lspconfig.nix
@@ -66,7 +66,7 @@ in
                     },
                 },
                 nixpkgs = {
-                    expr = 'import (builtins.getFlake \"${inputs.self}\").inputs.nixpkgs { }',
+                    expr = 'import <nixpkgs> { }',
                 },
                 options = {
                     nixos = {
