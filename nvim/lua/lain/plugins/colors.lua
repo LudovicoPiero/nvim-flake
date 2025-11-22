81daf434f2e392a5e4ae4b4b28d0e0c348f62ab1
Ludovico Piero <lewdovico@gnuweeb.org>
2025-11-23 05:22:11 +0900

    plugins/colors: change virtual_symbol
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- nvim/lua/lain/plugins/colors.lua
+++ nvim/lua/lain/plugins/colors.lua
@@ -4,7 +4,7 @@ require("nvim-highlight-colors").setup({
   render = "virtual",
 
   ---Set virtual symbol (requires render to be set to 'virtual')
-  virtual_symbol = "■",
+  virtual_symbol = "󱓻",
 
   ---Set virtual symbol suffix (defaults to '')
   virtual_symbol_prefix = "",
