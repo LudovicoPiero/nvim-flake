1df195e3ba16b5137e15d065fded26d675e94109
Ludovico Piero <lewdovico@gnuweeb.org>
2025-11-27 11:20:12 +0900

    colorscheme: change theme to catppuccin
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- nvim/lua/lain/core/colorscheme.lua
+++ nvim/lua/lain/core/colorscheme.lua
@@ -1,38 +1,55 @@
--- Lua
-require("onedark").setup({
-  -- Main options --
-  style = "deep", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-  transparent = false, -- Show/hide background
-  term_colors = true, -- Change terminal color as per the selected theme style
-  ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-  cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-  -- toggle theme style ---
-  toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-  toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
-
-  -- Change code style ---
-  -- Options are italic, bold, underline, none
-  -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
-  code_style = {
-    comments = "italic",
-    keywords = "bold",
-    functions = "italic",
-    strings = "none",
-    variables = "none",
+require("catppuccin").setup({
+  flavour = "mocha", -- latte, frappe, macchiato, mocha
+  background = { -- :h background
+    light = "latte",
+    dark = "mocha",
   },
-
-  -- Lualine options --
-  lualine = {
-    transparent = false, -- lualine center bar transparency
+  transparent_background = false, -- disables setting the background color.
+  float = {
+    transparent = false, -- enable transparent floating windows
+    solid = false, -- use solid styling for floating windows, see |winborder|
   },
-
-  -- Plugins Config --
-  diagnostics = {
-    darker = true, -- darker colors for diagnostic
-    undercurl = true, -- use undercurl instead of underline for diagnostics
-    background = true, -- use background color for virtual text
+  show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+  term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+  dim_inactive = {
+    enabled = false, -- dims the background color of inactive window
+    shade = "dark",
+    percentage = 0.15, -- percentage of the shade to apply to the inactive window
+  },
+  no_italic = false, -- Force no italic
+  no_bold = false, -- Force no bold
+  no_underline = false, -- Force no underline
+  lsp_styles = {
+    virtual_text = {
+      errors = { "italic" },
+      hints = { "italic" },
+      warnings = { "italic" },
+      information = { "italic" },
+      ok = { "italic" },
+    },
+    underlines = {
+      errors = { "underline" },
+      hints = { "underline" },
+      warnings = { "underline" },
+      information = { "underline" },
+      ok = { "underline" },
+    },
+    inlay_hints = {
+      background = true,
+    },
+  },
+  default_integrations = true,
+  auto_integrations = false,
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    notify = false,
+    mini = {
+      enabled = true,
+    },
   },
 })
 
-require("onedark").load()
+-- setup must be called before loading
+vim.cmd.colorscheme("catppuccin")
