ff35a1f5865885f4e1fa02f254643ab60810902d
Ludovico Piero <lewdovico@gnuweeb.org>
2026-06-01 16:36:32 +0900

    change autocmd for creating parent directory
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- nvim/lua/lain/core/autocmd.lua
+++ nvim/lua/lain/core/autocmd.lua
@@ -32,14 +32,13 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- Create parent directories on save.
-vim.api.nvim_create_autocmd("BufWritePre", {
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = augroup("auto_create_dir"),
   callback = function(event)
-    if event.match:match("^%w%w+://") then
+    if event.match:match("^%w%w+:[\\/][\\/]") then
       return
     end
-
     local file = vim.uv.fs_realpath(event.match) or event.match
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
